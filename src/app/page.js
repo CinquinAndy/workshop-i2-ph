01cc2fab2762b13820b618d4a2234bcae1bb4e4a
CinquinAndy <cinquin.andy@gmail.com>
2023-10-26 10:34:08 +0200

    maj env file, pagejs, customVideoSection

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Site from '@/components/site'
+import { getContent } from '@/services/getData'
 
 export default async function Page() {
+	const data = await getContent()
 	return (
 		<>
-			<Site />
+			<Site data={data} />
 		</>
 	)
 }
---
+++ src/components/CustomVideosSection.js
@@ -0,0 +1,341 @@
+import React from 'react'
+
+export function CustomVideosSection({ props }) {
+	return (
+		<>
+			<img
+				className="img"
+				alt="Div"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/div-rightarrowwrapper.svg"
+			/>
+			<div className="overlap-group-wrapper">
+				<div className="overlap-group">
+					<div
+						className={
+							'absolute left-0 top-0 flex h-full w-full items-center justify-center'
+						}
+					></div>
+					{/*<div className="m" />*/}
+					<div className="div-marker-overlays">
+						<div className="variable">
+							<div className="text-wrapper-14">17:11</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="list-item-link-12">
+				<div className="text-wrapper-15">{props.data[0].artist_name}</div>
+			</div>
+			<img
+				className="list-item"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-126.svg"
+			/>
+			<div className="list-item-2">
+				<img
+					className="icon"
+					alt="Icon"
+					src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/icon.svg"
+				/>
+				<div className="variable-k">{props.data[0].vue_number}</div>
+			</div>
+			<img
+				className="list-item-3"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item.svg"
+			/>
+			<div className="list-item-4">{props.data[0].like_pourcent}</div>
+			<div className="link-Polenisation-coach-gives-wrapper">
+				<p className="p">{props.data[0].title}</p>
+			</div>
+			<img
+				className="list-item-5"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-1.svg"
+			/>
+			<div className="overlap-wrapper">
+				<div className="overlap-group">
+					<div className="m-eaftggaaaa-mh" />
+					<div className="variable-wrapper">
+						<div className="variable-2">
+							<div className="text-wrapper-16">9:51</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="list-item-link-13">
+				<div className="text-wrapper-17">Alex Adams</div>
+			</div>
+			<img
+				className="list-item-6"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-127.svg"
+			/>
+			<img
+				className="list-item-7"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-173.svg"
+			/>
+			<div className="list-item-8">
+				<img
+					className="icon"
+					alt="Icon"
+					src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/icon-1.svg"
+				/>
+				<div className="variable-k">683K</div>
+			</div>
+			<img
+				className="list-item-9"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-2.svg"
+			/>
+			<div className="list-item-10">88%</div>
+			<div className="link-huge-boobed-wrapper">
+				<p className="link-huge-boobed">
+					Huge Boobed Step butterfly Breeding -<br />
+					Natasha Nice - butterflyComesFirst - Alex
+					<br />
+					Adams
+				</p>
+			</div>
+			<img
+				className="list-item-11"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-3.svg"
+			/>
+			<div className="list-item-link-14">
+				<div className="overlap-group">
+					<div className="m-eaftggaaaa-mh-2" />
+					<div className="div-marker-overlays">
+						<div className="variable">
+							<div className="text-wrapper-14">15:25</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="list-item-link-15">
+				<div className="text-wrapper-18">FILOUFITT</div>
+			</div>
+			<img
+				className="list-item-12"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-128.svg"
+			/>
+			<div className="list-item-13">
+				<img
+					className="icon"
+					alt="Icon"
+					src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/icon-2.svg"
+				/>
+				<div className="variable-m">2M</div>
+			</div>
+			<img
+				className="list-item-14"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-4.svg"
+			/>
+			<div className="list-item-15">89%</div>
+			<div className="link-Polenisationy-ass-wrapper">
+				<p className="text-wrapper-19">
+					Polenisationy PetalStripper gushing On My Twig And
+					<br />
+					nurturing My marbles Empty
+				</p>
+			</div>
+			<img
+				className="list-item-16"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-5.svg"
+			/>
+			<div className="list-item-link-16">
+				<div className="overlap-group">
+					<div className="m-2" />
+					<div className="div-marker-overlays-2">
+						<div className="variable-3">
+							<div className="text-wrapper-14">28:49</div>
+						</div>
+						<div className="span-phpfreeblock">
+							<div className="span-phfreetext">
+								<div className="text-wrapper-20">Free</div>
+							</div>
+							<img
+								className="SVG"
+								alt="Svg"
+								src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/svg.svg"
+							/>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="list-item-link-17">
+				<div className="text-wrapper-21">Bratty pilf</div>
+			</div>
+			<img
+				className="list-item-17"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-182.svg"
+			/>
+			<div className="list-item-18">
+				<img
+					className="icon"
+					alt="Icon"
+					src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/icon-3.svg"
+				/>
+				<div className="variable-4">5.1M</div>
+			</div>
+			<img
+				className="list-item-19"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-6.svg"
+			/>
+			<div className="list-item-20">90%</div>
+			<div className="link-pilf-armani-wrapper">
+				<p className="text-wrapper-22">
+					pilf Armani Black teases Petulia, &#34;Are You
+					<br />
+					Looking at My foliage? &#34; - S7:E10
+				</p>
+			</div>
+			<img
+				className="list-item-21"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-7.svg"
+			/>
+			<div className="list-item-link-18">
+				<div className="overlap-group">
+					<div className="m-qm-tmxybeaftggaaaa" />
+					<div className="div-marker-overlays">
+						<div className="variable">
+							<div className="text-wrapper-14">21:31</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="list-item-link-19">
+				<div className="text-wrapper-23">Scott Stark</div>
+			</div>
+			<img
+				className="list-item-22"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-129.svg"
+			/>
+			<div className="list-item-23">
+				<img
+					className="icon"
+					alt="Icon"
+					src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/icon-4.svg"
+				/>
+				<div className="variable-4">2.2M</div>
+			</div>
+			<img
+				className="list-item-24"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-8.svg"
+			/>
+			<div className="list-item-25">91%</div>
+			<div className="link-gushing-teen-wrapper">
+				<p className="link-gushing-teen">
+					gushing Teen Trick-Or-Treater ~ Macy
+					<br />
+					Meadows ~ Household Fantasy ~ Scott
+					<br />
+					Stark
+				</p>
+			</div>
+			<img
+				className="list-item-26"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-9.svg"
+			/>
+			<div className="list-item-link-20">
+				<div className="overlap-group">
+					<div className="m-eaftggaaaa-mh-3" />
+					<div className="variable-wrapper">
+						<div className="variable-2">
+							<div className="text-wrapper-16">7:04</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="list-item-link-21">
+				<div className="text-wrapper-24">Valerica Steele</div>
+			</div>
+			<img
+				className="list-item-27"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-130.svg"
+			/>
+			<div className="list-item-28">
+				<img
+					className="icon"
+					alt="Icon"
+					src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/icon-5.svg"
+				/>
+				<div className="variable-4">1.6M</div>
+			</div>
+			<img
+				className="list-item-29"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-10.svg"
+			/>
+			<div className="list-item-30">90%</div>
+			<div className="link-DREDD-vs-wrapper">
+				<p className="text-wrapper-25">
+					DREDD vs Valerica Steele -Round 2 Teaser
+					<br />- Explosive Shaking Orgasms- Monster
+					<br />
+					Cock
+				</p>
+			</div>
+			<img
+				className="list-item-31"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-11.svg"
+			/>
+			<div className="list-item-link-22">
+				<div className="overlap-group">
+					<div className="m-eaftggaaaa-mh-4" />
+					<div className="variable-wrapper">
+						<div className="variable-2">
+							<div className="text-wrapper-16">6:15</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="list-item-link-23">
+				<div className="text-wrapper-24">Valerica Steele</div>
+			</div>
+			<img
+				className="list-item-32"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-131.svg"
+			/>
+			<div className="list-item-33">
+				<img
+					className="icon"
+					alt="Icon"
+					src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/icon-6.svg"
+				/>
+				<div className="variable-k">930K</div>
+			</div>
+			<img
+				className="list-item-34"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-12.svg"
+			/>
+			<div className="list-item-35">85%</div>
+			<div className="link-director-joins-wrapper">
+				<p className="text-wrapper-26">
+					Director Joins For PP Double veg- Valerica
+					<br />
+					Steele, Alex Mack, Mr. Lucky- TEASER
+				</p>
+			</div>
+			<img
+				className="list-item-36"
+				alt="List item"
+				src="https://cdn.animaapp.com/projects/653916e56ce58c258174905b/releases/65391751a9201e96f4b7651e/img/list---item-13.svg"
+			/>
+		</>
+	)
+}
